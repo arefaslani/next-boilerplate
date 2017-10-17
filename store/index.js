596c6f091dafd840b3b53802a217a779de4b53a2
Aref Aslani <arefaslani@gmail.com>
2017-10-17 10:36:16 +0330

    add devtools extension in development

--- store/index.js
+++ store/index.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
-import withRedux from "next-redux-wrapper";
+import { createStore, applyMiddleware } from "redux"
+import withRedux from "next-redux-wrapper"
 import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 
 import reducer from 'store/reducer'
 
@@ -9,14 +10,13 @@ const middleWare = [];
 // Logger Middleware. This always has to be last
 const loggerMiddleware = createLogger({
   predicate: () => process.env.NODE_ENV === 'development',
-});
-middleWare.push(loggerMiddleware);
+})
+middleWare.push(loggerMiddleware)
 
-const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
+const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(...middleWare))(createStore)
 
 const makeStore = (initialState, options) => {
-  // return createStore(reducer, initialState, applyMiddleware(logger));
-  return createStoreWithMiddleware(reducer, initialState);
-};
+  return createStoreWithMiddleware(reducer, initialState)
+}
 
 export default makeStore
