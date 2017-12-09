8f7233f78f65c0224242db6a00c9e0ef229977ab
Aref Aslani <arefaslani@gmail.com>
2017-12-09 11:09:11 +0330

    add env vars support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-const path = require('path');
+const path = require('path')
+const { parsed: localEnv } = require('dotenv').config()
+const webpack = require('webpack')
+
 module.exports = {
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
     }
+
+    config.plugins.push(
+      new webpack.EnvironmentPlugin(process.env)
+    )
 
     config.module.rules.push(
       {
