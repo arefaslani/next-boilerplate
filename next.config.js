dbd26b2f1570396f666b67873e153cb12e32938e
Aref Aslani <arefaslani@gmail.com>
2017-12-11 00:04:05 +0330

    add local scss with hot reloading support

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { parsed: localEnv } = require('dotenv').config()
 const webpack = require('webpack')
 
 module.exports = {
-  webpack: (config) => {
+  webpack: (config, { dev }) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
@@ -13,27 +13,32 @@ module.exports = {
       new webpack.EnvironmentPlugin(process.env)
     )
 
-    config.module.rules.push(
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'emit-file-loader',
-            options: {
-              name: 'dist/[path][name].[ext].js',
-            },
-          },
-          {
-            loader: 'babel-loader',
-            options: {
-              babelrc: false,
-              extends: path.resolve(__dirname, './.babelrc'),
-            },
-          },
-          'styled-jsx-css-loader',
-        ],
-      }
-    );
+    config.module.rules.push({
+      test: /\.(sc|c)ss$/,
+      use: [
+        {
+          loader: "emit-file-loader",
+          options: {
+            name: "dist/[path][name].[ext].js"
+          }
+        },
+        {
+          loader: "babel-loader",
+          options: {
+            babelrc: false,
+            extends: path.resolve(__dirname, "./.babelrc")
+          }
+        },
+        "styled-jsx-css-loader",
+        { loader: "postcss-loader", options: { sourceMap: dev } },
+        {
+          loader: "sass-loader",
+          options: {
+            sourceMap: dev
+          }
+        }
+      ]
+    });
 
     return config
   }
