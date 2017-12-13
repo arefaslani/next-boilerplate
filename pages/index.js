97de74dddd49e8e2588ddfa9995c48fa065bbc58
Aref Aslani <arefaslani@gmail.com>
2017-12-13 23:24:10 +0330

    improve eslint configs

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import withRedux from 'next-redux-wrapper'
-import withReduxSaga from 'next-redux-saga'
-import Head from 'next/head'
-import styles from './index.scss'
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import withRedux from "next-redux-wrapper";
+import withReduxSaga from "next-redux-saga";
+import Head from "next/head";
+import PropTypes from "prop-types";
 
-import store from 'store'
-import { fetchPosts } from 'store/posts/actions'
-import { Link } from 'routes'
-import NProgress from 'components/NProgress'
+import Store from "store";
+import { fetchPosts } from "store/posts/actions";
+import { Link } from "routes";
+import NProgress from "components/NProgress";
+import styles from "./index.scss";
 
 class PostsIndex extends Component {
-  static getInitialProps({store}) {
-    store.dispatch(fetchPosts())
+  static getInitialProps({ store }) {
+    store.dispatch(fetchPosts());
   }
 
   render() {
-    const { posts, fetchPosts } = this.props
+    const { posts, fetchPostsAction } = this.props;
     return (
       <div>
         <style jsx>{styles}</style>
@@ -24,35 +25,47 @@ class PostsIndex extends Component {
           <title>Posts Index</title>
         </Head>
         <NProgress />
-        <button onClick={() => { fetchPosts() }}>Click Me</button>
+        <button
+          onClick={() => {
+            fetchPostsAction();
+          }}
+        >
+          Click Me
+        </button>
         <h1>Posts</h1>
-        { posts.length > 0 && posts.map(function(post) {
-          return (
+        {posts.length > 0 &&
+          posts.map(post => (
             <div key={post.id}>
               <h2>
-                <Link prefetch route='post' params={{ id: post.id }}>
+                <Link prefetch route="post" params={{ id: post.id }}>
                   <a>{post.title}</a>
                 </Link>
               </h2>
               <p>{post.body}</p>
             </div>
-          )
-        }) }
+          ))}
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    posts: state.posts
-  }
-}
+PostsIndex.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  fetchPostsAction: PropTypes.func.isRequired
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    fetchPosts
-  }, dispatch)
-}
+const mapStateToProps = state => ({
+  posts: state.posts
+});
 
-export default withRedux(store, mapStateToProps, mapDispatchToProps)(withReduxSaga(PostsIndex))
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      fetchPostsAction: fetchPosts
+    },
+    dispatch
+  );
+
+export default withRedux(Store, mapStateToProps, mapDispatchToProps)(
+  withReduxSaga(PostsIndex)
+);
